b18a29fca5b9ab63aaadc4ade2364fbb1a8b0be1
Dave Murray-Rust <dave@mo-seph.com>
2020-10-29 16:33:39 +0000

    Better comments

--- examples/ScreenTest/ScreenTest.ino
+++ examples/ScreenTest/ScreenTest.ino
@@ -1,9 +1,13 @@
 #include <DI.h>
 
 void setup() {
+  //Setup the screen - it will be an object called SCREEN
   startScreen();
-  showDISplash();
+
+  //Show an animated version of the DI Logo
+  animateDISplash();
 }
 
 void loop() {
+  //Nothing to do in the loop
 }
