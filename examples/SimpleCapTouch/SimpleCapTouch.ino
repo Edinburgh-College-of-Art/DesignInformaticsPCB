4a3f40fac76c39de72c544c0a6b1aed660a6a78f
mhamilt <matt.hamilton@ed.ac.uk>
2022-09-07 22:00:36 +0100

    Add simple Capacative Touch example

---
+++ examples/SimpleCapTouch/SimpleCapTouch.ino
@@ -0,0 +1,27 @@
+/*
+    Simple Capacative touch
+
+    This example demonstrates the  updating the pre-defined CAP_VALUE vairables.
+
+    Open the Serial Plotter to get a good idea as to the range of values
+
+    Pay attention to what range of values appear over time
+*/
+//-----------------------------------------------------------------
+#include <DI.h>
+//-----------------------------------------------------------------
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+  Serial.print("TOP"); Serial.print(' ');
+  Serial.print("MID"); Serial.print(' ');
+  Serial.println("BTM");
+}
+
+void loop() {
+  updateCapacitiveSensors();
+  Serial.print(CAP_VALUE_TOP); Serial.print(' ');
+  Serial.print(CAP_VALUE_MIDDLE); Serial.print(' ');
+  Serial.println(CAP_VALUE_BOTTOM);
+  delay(20);
+}
